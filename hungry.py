f7a69b52378cece59826404cd163a74370b65b1d
MOHIT SHARMA <mohitsharma6887@gmail.com>
2022-11-03 12:04:45 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -3,10 +3,9 @@ if hungry=="yes":
     print("eat samosa")
     print("pizza")
     print("burger")
-    print("fries")
+    print)"fries")
 else:
     thirsty=input("are u thristy")
     if thirsty=="yes":
         print("drink water")
-        print("drink soda")
     
